5914f9c5327263cbb1358693d0256bca48b65038
soo1e <gameswuon@gmail.com>
2025-02-08 16:14:57 +0900

    @Configuration과 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -6,34 +6,37 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.*;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
-    @Bean
-    public MemberService memberService() {
-        // 1번
-        System.out.println("call AppConfig.memberService");
-        return new MemberServiceImpl(memberRepository());
-    }
+	@Bean
+	public MemberService memberService() {
+		// 1번
+		System.out.println("call AppConfig.memberService");
+		return new MemberServiceImpl(memberRepository());
+	}
 
-    @Bean
-    public MemberRepository memberRepository() {
-        // 2번? 3번?
-        System.out.println("call AppConfig.memberRepository");
-        return new MemoryMemberRepository();
-    }
+	@Bean
+	public OrderService orderService() {
+		System.out.println("call AppConfig.orderService");
+		return new OrderServiceImpl(
+			memberRepository(),
+			discountPolicy());
+	}
 
-    @Bean
-    public OrderService orderService() {
-        System.out.println("AppConfig.orderService");
-        return new OrderServiceImpl(memberRepository(), discountPolicy());
-    }
+	@Bean
+	public MemberRepository memberRepository() {
+		//2번? 3번?
+		System.out.println("call AppConfig.memberRepository");
+		return new MemoryMemberRepository();
+	}
 
-    @Bean
-    public DiscountPolicy discountPolicy() {
-        return new RateDiscountPolicy();
-    }
+	@Bean
+	public DiscountPolicy discountPolicy() {
+		return new RateDiscountPolicy();
+	}
 }
